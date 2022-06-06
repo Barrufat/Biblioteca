a020a592f5c7828d6716849c56d52c133a0ddc19
barrufat <barrufat@hotmail.com>
2022-06-06 18:31:31 +0200

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 
 function App() {
@@ -6,56 +5,79 @@ function App() {
 
     <div className="container">
       <div className="columnA">
-      <img className="casilla" src="./A1.png" alt="A1" />
-      <img className="casilla" src="./A2.png" alt="A2" />
-      <img className="casilla" src="./A3.png" alt="A3" />
-      <img className="casilla" src="./A4.png" alt="A4" />
-      <img className="casilla" src="./A5.png" alt="A5" />
-      <img className="casilla" src="./A6.png" alt="A6" />
-      <img className="casilla" src="./A7.png" alt="A7" />
-      <img className="casilla" src="./A8.png" alt="A8" />
-      <img className="casilla" src="./A9.png" alt="A9" />
-      <img className="casilla" src="./A10.png" alt="A10" />
+        <img className="casilla" src="./A1.png" alt="A1" />
+        <img className="casilla" src="./A2.png" alt="A2"  />
+        <img className="casilla" src="./A3.png" alt="A3" />
+        <img className="casilla" src="./A4.png" alt="A4" />
+        <img className="casilla" src="./A5.png" alt="A5" />
+        <img className="casilla" src="./A6.png" alt="A6" />
+        <img className="casilla" src="./A7.png" alt="A7" />
+        <img className="casilla" src="./A8.png" alt="A8" />
+        <img className="casilla" src="./A9.png" alt="A9" />
+        <img className="casilla" src="./A10.png" alt="A10" />
       </div>
       <div className="columnB">
-      <img className="casilla" src="./B1.png" alt="B1" />
-      <img className="casilla" src="./B2.png" alt="B2" />
-      <img className="casilla" src="./B3.png" alt="B3" />
-      <img className="casilla" src="./B4.png" alt="B4" />
-      <img className="casilla" src="./B5.png" alt="B5" />
-      <img className="casilla" src="./B6.png" alt="B6" />
-      <img className="casilla" src="./B7.png" alt="B7" />
-      <img className="casilla" src="./B8.png" alt="B8" />
-      <img className="casilla" src="./B9.png" alt="B9" />
-      <img className="casilla" src="./B10.png" alt="B10" />
+        <img className="casilla, B1" src="./B1.png" alt="B1" />
+        <img className="casilla, B2" src="./B2.png" alt="B2" />
+        <img className="casilla, B3" src="./B3.png" alt="B3" />
+        <img className="casilla, B4" src="./B4.png" alt="B4" />
+        <img className="casilla, B5" src="./B5.png" alt="B5" />
+        <img className="casilla, B6" src="./B6.png" alt="B6" />
+        <img className="casilla, B7" src="./B7.png" alt="B7" />
+        <img className="casilla, B8" src="./B8.png" alt="B8" />
+        <img className="casilla, B9" src="./B9.png" alt="B9" />
+        <img className="casilla, B10" src="./B10.png" alt="B10" />
       </div>
       <div className="columnC">
-      <img className="casilla" src="./C1.png" alt="C1" />
-      <img className="casilla" src="./C2.png" alt="C2" />
-      <img className="casilla" src="./C3.png" alt="C3" />
-      <img className="casilla" src="./C4.png" alt="C4" />
-      <img className="casilla" src="./C5.png" alt="C5" />
-      <img className="casilla" src="./C6.png" alt="C6" />
-      <img className="casilla" src="./C7.png" alt="C7" />
-      <img className="casilla" src="./C8.png" alt="C8" />
-      <img className="casilla" src="./C9.png" alt="C9" />
-      <img className="casilla" src="./C10.png" alt="C10" />
+        <img className="casilla" src="./C1.png" alt="C1" />
+        <img className="casilla" src="./C2.png" alt="C2" />
+        <img className="casilla" src="./C3.png" alt="C3" />
+        <img className="casilla" src="./C4.png" alt="C4" />
+        <img className="casilla" src="./C5.png" alt="C5" />
+        <img className="casilla" src="./C6.png" alt="C6" />
+        <img className="casilla" src="./C7.png" alt="C7" />
+        <img className="casilla" src="./C8.png" alt="C8" />
+        <img className="casilla" src="./C9.png" alt="C9" />
+        <img className="casilla" src="./C10.png" alt="C10" />
       </div>
       <div className="columnD">
-      <img className="casilla" src="./D1.png" alt="D1" />
-      <img className="casilla" src="./D2.png" alt="D2" />
-      <img className="casilla" src="./D3.png" alt="D3" />
-      <img className="casilla" src="./D4.png" alt="D4" />
-      <img className="casilla" src="./D5.png" alt="D5" />
-      <img className="casilla" src="./D6.png" alt="D6" />
-      <img className="casilla" src="./D7.png" alt="D7" />
-      <img className="casilla" src="./D8.png" alt="D8" />
-      <img className="casilla" src="./D9.png" alt="D9" />
-      <img className="casilla" src="./D10.png" alt="D10" />
+        <img className="casilla" src="./D1.png" alt="D1" />
+        <img className="casilla" src="./D2.png" alt="D2" />
+        <img className="casilla" src="./D3.png" alt="D3" />
+        <img className="casilla" src="./D4.png" alt="D4" />
+        <img className="casilla" src="./D5.png" alt="D5" />
+        <img className="casilla" src="./D6.png" alt="D6" />
+        <img className="casilla" src="./D7.png" alt="D7" />
+        <img className="casilla" src="./D8.png" alt="D8" />
+        <img className="casilla" src="./D9.png" alt="D9" />
+        <img className="casilla" src="./D10.png" alt="D10" />
+      </div>
+      <div className="columnE">
+        <img className="casilla" src="./E1.png" alt="E1" />
+        <img className="casilla" src="./E2.png" alt="E2" />
+        <img className="casilla" src="./E3.png" alt="E3" />
+        <img className="casilla" src="./E4.png" alt="E4" />
+        <img className="casilla" src="./E5.png" alt="E5" />
+        <img className="casilla" src="./E6.png" alt="E6" />
+        <img className="casilla" src="./E7.png" alt="E7" />
+        <img className="casilla" src="./E8.png" alt="E8" />
+        <img className="casilla" src="./E9.png" alt="E9" />
+        <img className="casilla" src="./E10.png" alt="E10" />
+      </div>
+      <div className="columnF">
+        <img className="casilla" src="./F1.png" alt="F1" />
+        <img className="casilla" src="./F2.png" alt="F2" />
+        <img className="casilla" src="./F3.png" alt="F3" />
+        <img className="casilla" src="./F4.png" alt="F4" />
+        <img className="casilla" src="./F5.png" alt="F5" />
+        <img className="casilla" src="./F6.png" alt="F6" />
+        <img className="casilla" src="./F7.png" alt="F7" />
+        <img className="casilla" src="./F8.png" alt="F8" />
+        <img className="casilla" src="./F9.png" alt="F9" />
       </div>
     </div>
 
-  );
+);
 }
 
 export default App;
